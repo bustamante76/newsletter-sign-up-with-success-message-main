46860828a1687d355776abcfae53715c1a8e58a3
bustamante76 <124016811+bustamante76@users.noreply.github.com>
2025-08-08 18:44:59 -0500

    commit init

---
+++ js/script.js
@@ -0,0 +1,35 @@
+let formEmail = document.querySelector(".form__email");
+let formButton = document.querySelector(".form__button");
+let spanError = document.querySelector(".span");
+let cardSuccess = document.querySelector(".card-success");
+let card = document.querySelector(".card");
+
+formButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(formEmail.value)
+    if (formEmail.value === "") {
+        formEmail.classList.add("email--error");
+        formEmail.placeholder = "Please enter your email";
+        spanError.style.display = "block";
+        cardSuccess.style.display = "none";
+        card.style.display = "flex";
+    } else {
+        if (formEmail.validity.valid) {
+            formEmail.classList.remove("email--error");
+            formEmail.placeholder = "email@company.com";
+            spanError.style.display = "none";
+            cardSuccess.style.display = "flex";
+            card.style.display = "none";
+        } else {
+            formEmail.classList.add("email--error");
+            spanError.style.display = "initial";
+            cardSuccess.style.display = "none";
+            card.style.display = "flex";
+        }
+    }
+});
+
+cardSuccess.addEventListener("click", () => {
+    cardSuccess.style.display = "none";
+    card.style.display = "flex";
+ });
